70cab95342941a4ffea11e4682602234c4b1b79d
Ethan <ethanjayde@gmail.com>
2019-10-05 11:37:13 -0400

    Added color sensor telemetry to tele-op
    Signed-off-by: DRSS Enterprise <ethanjayde@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Example/Modular_Mechanum_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Example/Modular_Mechanum_Drive.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.Example;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
 
 @TeleOp(name = "Modular_Mechanum_Drive", group= "Modular_Drivetrains")
 public class Modular_Mechanum_Drive extends OpMode {
@@ -12,6 +20,8 @@ public class Modular_Mechanum_Drive extends OpMode {
     public DcMotor motorFrontLeft;
     public DcMotor motorBackRight;
     public DcMotor motorBackLeft;
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
     double Speed = 1;
     double Frontleft;
     double Frontright;
@@ -37,6 +47,9 @@ public class Modular_Mechanum_Drive extends OpMode {
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        sensorColor = hardwareMap.get(ColorSensor.class, "color_sensor");
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "color_sensor");
     }
 
     @Override
@@ -85,6 +98,34 @@ public class Modular_Mechanum_Drive extends OpMode {
         if (gamepad1.y){
             Speed = .25;
         }
+
+        //Color Sensor Code
+
+            //Color Sensor Values
+            // hsvValues is an array that will hold the hue, saturation, and value information.
+            float hsvValues[] = {0F, 0F, 0F};
+
+            // values is a reference to the hsvValues array.
+            final float values[] = hsvValues;
+
+            // sometimes it helps to multiply the raw RGB values with a scale factor
+            // to amplify/attentuate the measured values.
+            final double SCALE_FACTOR = 255;
+
+        // convert the RGB values to HSV values.
+            // multiply by the SCALE_FACTOR.
+            // then cast it back to int (SCALE_FACTOR is a double)
+            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                    (int) (sensorColor.green() * SCALE_FACTOR),
+                    (int) (sensorColor.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
 
         /*
          * Telemetry for debugging
