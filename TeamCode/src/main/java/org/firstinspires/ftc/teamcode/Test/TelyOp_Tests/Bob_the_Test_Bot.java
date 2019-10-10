73afdf5f721fb2f15bc257b0e5052ae2373aa45c
Ethan <ethanjayde@gmail.com>
2019-10-10 10:12:02 -0400

    Comments
    Signed-off-by: Ethan Shober <ethanjayde@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/Bob_the_Test_Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/Bob_the_Test_Bot.java
@@ -1,3 +1,11 @@
+
+/* CONTROLS USED:
+        Controller one:
+            Left stick, Right stick x, DPad, a, b, x, y
+        Controller two:
+            Left stick y, a, b, Bumpers, Triggers
+ */
+
 // Defines where the code is in the project
 package org.firstinspires.ftc.teamcode.Test.TelyOp_Tests;
 
