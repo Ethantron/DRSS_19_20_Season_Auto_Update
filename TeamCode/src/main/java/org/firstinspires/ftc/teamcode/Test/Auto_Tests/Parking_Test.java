c9a2a6dd43bc1252e322920a8b6b45abaf776965
Ethan <ethanjayde@gmail.com>
2019-10-10 10:08:31 -0400

    Comments
    Signed-off-by: Ethan Shober <ethanjayde@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Parking_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Parking_Test.java
@@ -1,17 +1,19 @@
+// Defines where the code is in the project
 package org.firstinspires.ftc.teamcode.Test.Auto_Tests;
 
+// Imports for the code
 import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
+// Defines the codes name
 @Autonomous(name = "Parking_Test", group= "Auto_Tests")
 public class Parking_Test extends LinearOpMode{
 
+    // Robot definitions
     public DcMotor motorFrontRight;
     public DcMotor motorFrontLeft;
     public DcMotor motorBackRight;
@@ -23,46 +25,49 @@ public class Parking_Test extends LinearOpMode{
     @Override
     public void runOpMode(){
 
+        // Drive train initialization
         motorFrontRight = hardwareMap.dcMotor.get("FR");
-        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
-
         motorFrontLeft = hardwareMap.dcMotor.get("FL");
-        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
-
         motorBackLeft = hardwareMap.dcMotor.get("BL");
-        motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
-
         motorBackRight = hardwareMap.dcMotor.get("BR");
+
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
 
+        // Color sensor initialization
         sensorColor = hardwareMap.get(ColorSensor.class, "color_sensor");
         sensorDistance = hardwareMap.get(DistanceSensor.class, "color_sensor");
 
-        //Color Sensor Values
-        // hsvValues is an array that will hold the hue, saturation, and value information.
+        // Color Sensor Values
+        // "hsvValues" is an array that will hold the hue, saturation, and value information
         float hsvValues[] = {0F, 0F, 0F};
 
-        // values is a reference to the hsvValues array.
+        // "values" is a reference to the hsvValues array
         final float values[] = hsvValues;
 
-        // sometimes it helps to multiply the raw RGB values with a scale factor
-        // to amplify/attentuate the measured values.
+        // Sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attenuate the measured values.
         final double SCALE_FACTOR = 255;
 
+        telemetry.addData("Status: ", "Initialized");
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
         waitForStart();
 
         while (opModeIsActive()){
-            //Color Sensor Code
+            // Color Sensor Code
 
-            // convert the RGB values to HSV values.
-            // multiply by the SCALE_FACTOR.
-            // then cast it back to int (SCALE_FACTOR is a double)
+            // Convert the RGB values to HSV values.
+            // Multiply by the SCALE_FACTOR.
+            // Cast it back to int (SCALE_FACTOR is a double)
             Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
                     (int) (sensorColor.green() * SCALE_FACTOR),
                     (int) (sensorColor.blue() * SCALE_FACTOR),
                     hsvValues);
 
-            // send the info back to driver station using telemetry function.
+            // Send the info back to driver station using telemetry function.
             telemetry.addData("Step: ", step);
             telemetry.addData("Alpha", sensorColor.alpha());
             telemetry.addData("Red  ", sensorColor.red());
@@ -71,32 +76,23 @@ public class Parking_Test extends LinearOpMode{
             telemetry.addData("Hue", hsvValues[0]);
             telemetry.update();
 
-            //Orient the Robot (Gyro Code)
+            // Move forward
             if (step == 0){
-                //Turn using Gyro
-                step++;
-            }
-
-            //Move forward
-            if (step == 1){
                 motorFrontRight.setPower(.3);
                 motorFrontLeft.setPower(.3);
                 motorBackLeft.setPower(.3);
                 motorBackRight.setPower(.3);
             }
 
-            //Does it see the line?
-            while (step == 1){
+            // Does it see the line?
+            while (step == 0){
                 if (opModeIsActive()){
-                        // convert the RGB values to HSV values.
-                        // multiply by the SCALE_FACTOR.
-                        // then cast it back to int (SCALE_FACTOR is a double)
                         Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
                                 (int) (sensorColor.green() * SCALE_FACTOR),
                                 (int) (sensorColor.blue() * SCALE_FACTOR),
                                 hsvValues);
 
-                        // send the info back to driver station using telemetry function.
+                        // Send the info back to driver station using telemetry function.
                         telemetry.addData("Step: ", step);
                         telemetry.addData("Alpha", sensorColor.alpha());
                         telemetry.addData("Red  ", sensorColor.red());
@@ -104,9 +100,8 @@ public class Parking_Test extends LinearOpMode{
                         telemetry.addData("Blue ", sensorColor.blue());
                         telemetry.addData("Hue", hsvValues[0]);
                         telemetry.update();
-
                 }
-                if (hsvValues[0] > 100 || sensorColor.blue() > 100){ //Checks if it is red or blue
+                if (hsvValues[0] > 100 || sensorColor.blue() > 100){ // Checks if it is red or blue
                     step++;
                 }
             }
@@ -117,10 +112,7 @@ public class Parking_Test extends LinearOpMode{
                 motorBackLeft.setPower(0);
                 motorBackRight.setPower(0);
             }
-
-
         }
-
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/Bob_the_Test_Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/Bob_the_Test_Bot.java
@@ -1,14 +1,18 @@
+// Defines where the code is in the project
 package org.firstinspires.ftc.teamcode.Test.TelyOp_Tests;
 
+// Imports for the code
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+// Defines the codes name
 @TeleOp(name = "Bob_the_Test_Bot", group= "TelyOp_Tests")
 public class Bob_the_Test_Bot extends LinearOpMode{
 
+    // Robot definitions
     public DcMotor motorFrontRight;
     public DcMotor motorFrontLeft;
     public DcMotor motorBackRight;
@@ -28,7 +32,7 @@ public class Bob_the_Test_Bot extends LinearOpMode{
 
     public void runOpMode(){
 
-        // Drive Train initilization
+        // Drive Train initialization
         motorFrontRight = hardwareMap.dcMotor.get("FR");
         motorFrontLeft = hardwareMap.dcMotor.get("FL");
         motorBackLeft = hardwareMap.dcMotor.get("BL");
@@ -58,8 +62,8 @@ public class Bob_the_Test_Bot extends LinearOpMode{
 
         // Drivetrain Controls
 
-            // left stick controls direction
-            // right stick X controls rotation
+            // Left stick controls direction
+            // Right stick X controls rotation
             float gamepad1LeftY = gamepad1.left_stick_y;
             float gamepad1LeftX = -gamepad1.left_stick_x;
             float gamepad1RightX = gamepad1.right_stick_x;
@@ -70,51 +74,57 @@ public class Bob_the_Test_Bot extends LinearOpMode{
             double BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
             double BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
 
-            // clip the right/left values so that the values never exceed +/- 1
-
-            // write the values to the motors
+            // Write the values to the motors
             motorFrontRight.setPower(Frontright);
             motorFrontLeft.setPower(Frontleft);
             motorBackLeft.setPower(Backleft);
             motorBackRight.setPower(Backright);
 
-            // sets speed
+            // Sets speed
             Frontright = Range.clip(Math.pow(FrontRight, 3), -Speed, Speed);
             Frontleft = Range.clip(Math.pow(FrontLeft, 3), -Speed, Speed);
             Backright = Range.clip(Math.pow(BackRight, 3), -Speed, Speed);
             Backleft = Range.clip(Math.pow(BackLeft, 3), -Speed, Speed);
 
+            // Full speed
             if (gamepad1.a){
                 Speed = 1;
             }
 
+            // Three quarters speed
             if (gamepad1.b){
                 Speed = .75;
             }
 
+            // Half speed
             if (gamepad1.x){
                 Speed = .50;
             }
 
+            // Quarter speed
             if (gamepad1.y){
                 Speed = .25;
             }
 
+            // Sets the foundation movers for moving left
             if (gamepad1.dpad_left){
                 grabL.setPosition(90);
                 grabR.setPosition(0);
             }
 
+            // Sets the foundation movers for moving right
             if (gamepad1.dpad_right){
                 grabR.setPosition(90);
                 grabL.setPosition(0);
             }
 
+            // Lowers foundation movers
             if (gamepad1.dpad_down){
                 grabL.setPosition(0);
                 grabR.setPosition(0);
             }
 
+            // Raises the foundation movers
             if (gamepad1.dpad_up){
                 grabL.setPosition(90);
                 grabR.setPosition(90);
@@ -122,6 +132,7 @@ public class Bob_the_Test_Bot extends LinearOpMode{
 
         // Payload Controls
 
+            // Moves the lift up by one stone
             if (gamepad2.right_bumper && upcount <= 6){
                 lift.setPower(0.5);
                 sleep(500);
@@ -129,9 +140,10 @@ public class Bob_the_Test_Bot extends LinearOpMode{
                 upcount++;
             }
             else{
-                telemetry.addData("Maximum","Hight Reached");
+                telemetry.addData("Maximum","Height Reached");
             }
 
+            // Moves the lift down by one stone
             if (gamepad2.left_bumper && upcount >= 0){
                 lift.setPower(-0.3);
                 sleep(500);
@@ -139,42 +151,50 @@ public class Bob_the_Test_Bot extends LinearOpMode{
                 upcount--;
             }
 
+            // Moves the lift up
             if (gamepad2.left_trigger > 0.1){
                 lift.setPower(0.5);
             }
 
+            // Moves the lift down
             if (gamepad2.right_trigger > 0.1){
                 lift.setPower(-0.3);
             }
 
+            // If neither triggers are being pressed, keep the lift still
             if (!(gamepad2.left_trigger > 0.1 && gamepad2.right_trigger > 0.1)){
                 lift.setPower(0.1);
             }
 
+            // Moves the slide forward
             if (gamepad2.left_stick_x > 0.1){
                 slide.setPower(0.3);
             }
 
+            // Moves the slide backwards
             if (gamepad2.left_stick_x < 0.1){
                 slide.setPower(-0.3);
             }
 
+            // If the stick is centered, keep the slide still
             if (!(gamepad2.left_stick_x > 0.1) && !(gamepad2.left_stick_x < 0.1)){
                 slide.setPower(0);
             }
 
+            // Opens the stone grabber
             if (gamepad2.a){
                 grabStone.setPosition(90);
             }
 
+            // Closes the stone grabber
             if (gamepad2.b){
                 grabStone.setPosition(0);
             }
 
-        //Telemetry
-
+        // Telemetry
         telemetry.addData("Current Speed: ", Speed);
 
+        // Left grabber telemetry
         if (grabL.getPosition() == 90){
             telemetry.addData("Left Grabber: ", "Open");
         }
@@ -182,6 +202,7 @@ public class Bob_the_Test_Bot extends LinearOpMode{
             telemetry.addData("Left Grabber: ", "Closed");
         }
 
+        // Right grabber telemetry
         if (grabR.getPosition() == 90){
             telemetry.addData("Right Grabber: ", "Open");
         }
@@ -189,6 +210,7 @@ public class Bob_the_Test_Bot extends LinearOpMode{
             telemetry.addData("Right Grabber: ", "Closed");
         }
 
+        // Stone grabber telemetry
         if (grabStone.getPosition() == 90){
             telemetry.addData("Stone Grabber: ", "Open");
         }
@@ -198,29 +220,29 @@ public class Bob_the_Test_Bot extends LinearOpMode{
         telemetry.update();
     }
 
-    /*
-     * This method scales the joystick input so for low joystick values, the
-     * scaled value is less than linear.  This is to make it easier to drive
-     * the robot more precisely at slower speeds.
-     */
+
+    /* This method scales the joystick input so for low joystick values, the
+       scaled value is less than linear.  This is to make it easier to drive
+       the robot more precisely at slower speeds. */
+
     public double scaleInput(double dVal){
         double[] scaleArray ={ 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
 
-        // get the corresponding index for the scaleInput array.
+        // Gets the corresponding index for the scaleInput array.
         int index = (int) (dVal * 16.0);
 
-        // index should be positive.
+        // The index should be positive.
         if (index < 0){
             index = -index;
         }
 
-        // index cannot exceed size of array minus 1.
+        // The index cannot exceed size of array minus 1.
         if (index > 16){
             index = 16;
         }
 
-        // get value from the array.
+        // Get value from the array.
         double dScale;
         if (dVal < 0){
             dScale = -scaleArray[index];
@@ -228,7 +250,7 @@ public class Bob_the_Test_Bot extends LinearOpMode{
             dScale = scaleArray[index];
         }
 
-        // return scaled value.
+        // Returns the scaled value.
         return dScale;
     }
 }
