e90e60aec48db5f8d870bad665463a54a3527cf7
DRSS Enterprise <ethanjayde@gmail.com>
2019-10-22 18:54:54 -0400

    Copied the vuforia from webcam for testing with snake cam
    Signed-off-by: DRSS Enterprise <ethanjayde@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Auto_Scanning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Auto_Scanning.java
@@ -367,32 +367,32 @@ public class Auto_Scanning extends LinearOpMode {
     }
 
     public void scan (){
-            if (tfod != null) {
-                // getUpdatedRecognitions() will return null if no new information is available since
-                // the last time that call was made.
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-
-                    // step through the list of recognitions and display boundary info.
-                    int i = 0;
-                    for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-                        if (recognition.getLabel() == LABEL_SECOND_ELEMENT){
-                            Skystone = true;
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                            if (recognition.getLabel() == LABEL_SECOND_ELEMENT){
+                                Skystone = true;
+                            }
                         }
+                        telemetry.update();
                     }
-                    telemetry.update();
                 }
+                sleep(4000);
             }
-            sleep(4000);
-//            if (tfod != null) {
-//                tfod.shutdown();
-//            }
+        }
     }
 
 
