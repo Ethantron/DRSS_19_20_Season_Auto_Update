0167ebdb0667423c09572caafd3988093b17fedc
DRSS Enterprise <ethanjayde@gmail.com>
2019-11-09 10:35:45 -0500

    Updated 19-20_TeleOp to newest version
    Signed-off-by: gagne-3 <gagne.3@daytonstemschool.org>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/TeleOp_19_20.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TelyOp_Tests/TeleOp_19_20.java
@@ -12,28 +12,30 @@ public class TeleOp_19_20 extends OpMode {
     // Robot definitions
 
     // Motor Definitions
-        public DcMotor motorFrontRight;
-        public DcMotor motorFrontLeft;
-        public DcMotor motorBackRight;
-        public DcMotor motorBackLeft;
+    public DcMotor motorFrontRight;
+    public DcMotor motorFrontLeft;
+    public DcMotor motorBackRight;
+    public DcMotor motorBackLeft;
 
     // Mechanum Definitions
-        double Frontleft;
-        double Frontright;
-        double Backleft;
-        double Backright;
-        double Speed = 1;
+    double Frontleft;
+    double Frontright;
+    double Backleft;
+    double Backright;
+    double Speed = 1;
 
     // Payload Definitions
-        public DcMotor lift;
-        public DcMotor slide;
-        public Servo grabStone;
-        public Servo wrist;
-        public Servo FoundationMoverL;
-        public Servo FoundationMoverR;
-        //Lift Positioning Definitions
-            double upstep = 0;
-            double upcount = 0;
+    public DcMotor lift;
+    public DcMotor slide;
+    public Servo grabStone;
+    public Servo wrist;
+    public Servo FoundationMoverL;
+    public Servo FoundationMoverR;
+    //Lift Positioning Definitions
+    double upstep = 0;
+    double upcount = 0;
+
+    double claw_status = 1;
 
     // End of Definitions
 
@@ -42,46 +44,46 @@ public class TeleOp_19_20 extends OpMode {
 
         /** Drive Train initialization **/
 
-            //Motor Initialization
-                motorFrontRight = hardwareMap.dcMotor.get("FR");
-                motorFrontLeft = hardwareMap.dcMotor.get("FL");
-                motorBackLeft = hardwareMap.dcMotor.get("BL");
-                motorBackRight = hardwareMap.dcMotor.get("BR");
+        //Motor Initialization
+        motorFrontRight = hardwareMap.dcMotor.get("FR");
+        motorFrontLeft = hardwareMap.dcMotor.get("FL");
+        motorBackLeft = hardwareMap.dcMotor.get("BL");
+        motorBackRight = hardwareMap.dcMotor.get("BR");
 
-            //Motor Drection Initialization
-                motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
-                motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
-                motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
-                motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+        //Motor Drection Initialization
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
 
-            //Initialized Telemetry
-                telemetry.addData("Drive Train: ", "Initialized");
-                telemetry.update();
+        //Initialized Telemetry
+        telemetry.addData("Drive Train: ", "Initialized");
+        telemetry.update();
 
         /** End of Drive Train Initialization **/
 
         /** Payload Initialization **/
 
-            //Lift Initialization
-                lift = hardwareMap.dcMotor.get("LT");
-                lift.setDirection(DcMotor.Direction.FORWARD);
+        //Lift Initialization
+        lift = hardwareMap.dcMotor.get("LT");
+        lift.setDirection(DcMotor.Direction.FORWARD);
 
-            //Slide Initiailization
-                slide = hardwareMap.dcMotor.get("SL");
-                slide.setDirection(DcMotor.Direction.FORWARD);
+        //Slide Initiailization
+        slide = hardwareMap.dcMotor.get("SL");
+        slide.setDirection(DcMotor.Direction.FORWARD);
 
-            //Hand Initiailization
-                grabStone = hardwareMap.servo.get("GS");
-                wrist = hardwareMap.servo.get("W");
-                //wrist.setPosition(0.5); // Center the wrist
+        //Hand Initiailization
+        grabStone = hardwareMap.servo.get("GS");
+        wrist = hardwareMap.servo.get("W");
+        //wrist.setPosition(0.5); // Center the wrist
 
-            //Foundation Mover Initialization
-                FoundationMoverL = hardwareMap.servo.get("GL");
-                FoundationMoverR = hardwareMap.servo.get("GR");
+        //Foundation Mover Initialization
+        FoundationMoverL = hardwareMap.servo.get("GL");
+        FoundationMoverR = hardwareMap.servo.get("GR");
 
-            //Initialized Telemetry
-                telemetry.addData("Payload: ", "Initialized");
-                telemetry.update();
+        //Initialized Telemetry
+        telemetry.addData("Payload: ", "Initialized");
+        telemetry.update();
 
         /** End of Payload Initiailization **/
 
@@ -96,140 +98,141 @@ public class TeleOp_19_20 extends OpMode {
         /** Gamepad 1 Controls (Drive Train) ==> **/
 
         /**Mechanum Drive Controls**/
-            // left stick controls direction
-            // right stick X controls rotation
-                float gamepad1LeftY = gamepad1.left_stick_y;
-                float gamepad1LeftX = -gamepad1.left_stick_x;
-                float gamepad1RightX = -gamepad1.right_stick_x;
+        // left stick controls direction
+        // right stick X controls rotation
+        float gamepad1LeftY = gamepad1.left_stick_y;
+        float gamepad1LeftX = -gamepad1.left_stick_x;
+        float gamepad1RightX = -gamepad1.right_stick_x;
 
-            // Mechanum formulas
-                double FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-                double FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-                double BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
-                double BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+        // Mechanum formulas
+        double FrontLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+        double FrontRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+        double BackRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+        double BackLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
 
-            // clip the right/left values so that the values never exceed +/- 1
+        // clip the right/left values so that the values never exceed +/- 1
 
-            // write the values to the motors
-                motorFrontRight.setPower(Frontright);
-                motorFrontLeft.setPower(Frontleft);
-                motorBackLeft.setPower(Backleft);
-                motorBackRight.setPower(Backright);
+        // write the values to the motors
+        motorFrontRight.setPower(Frontright);
+        motorFrontLeft.setPower(Frontleft);
+        motorBackLeft.setPower(Backleft);
+        motorBackRight.setPower(Backright);
 
-            // sets speed
-                Frontright = Range.clip(Math.pow(FrontRight, 3), -Speed, Speed);
-                Frontleft = Range.clip(Math.pow(FrontLeft, 3), -Speed, Speed);
-                Backright = Range.clip(Math.pow(BackRight, 3), -Speed, Speed);
-                Backleft = Range.clip(Math.pow(BackLeft, 3), -Speed, Speed);
+        // sets speed
+        Frontright = Range.clip(Math.pow(FrontRight, 3), -Speed, Speed);
+        Frontleft = Range.clip(Math.pow(FrontLeft, 3), -Speed, Speed);
+        Backright = Range.clip(Math.pow(BackRight, 3), -Speed, Speed);
+        Backleft = Range.clip(Math.pow(BackLeft, 3), -Speed, Speed);
 
-            //Speed Controls
-                if (gamepad1.a){
-                    Speed = 1; // Full Speed
-                }
+        //Speed Controls
+        if (gamepad1.a){
+            Speed = 1; // Full Speed
+        }
 
-                if (gamepad1.b){
-                    Speed = .75; // Three Quarter Speed
-                }
+        if (gamepad1.b){
+            Speed = .75; // Three Quarter Speed
+        }
 
-                if (gamepad1.x){
-                    Speed = .50; // Half Speed
-                }
+        if (gamepad1.x){
+            Speed = .50; // Half Speed
+        }
 
-                if (gamepad1.y){
-                    Speed = .25; // Quarter Speed
-                }
-            //End of Speed Controls
+        if (gamepad1.y){
+            Speed = .25; // Quarter Speed
+        }
+        //End of Speed Controls
         /** End of Mechanum Drive Controls **/
 
         /** Foundation Mover Controls **/
-            // Lowering Foundation Movers
-                if (gamepad1.dpad_down){
-                    FoundationMoverL.setPosition(1);
-                    FoundationMoverR.setPosition(1);
-                }
-            // End of Lowering Foundation Movers
+        // Lowering Foundation Movers
+        if (gamepad1.dpad_down){
+            FoundationMoverL.setPosition(1);
+            FoundationMoverR.setPosition(1);
+        }
+        // End of Lowering Foundation Movers
 
-            // Raising Foundation Movers
-                if (gamepad1.dpad_up){
-                    FoundationMoverL.setPosition(0);
-                    FoundationMoverR.setPosition(0);
-                }
+        // Raising Foundation Movers
+        if (gamepad1.dpad_up){
+            FoundationMoverL.setPosition(0);
+            FoundationMoverR.setPosition(0);
+        }
 
-                telemetry.addData("Speed", Speed);
-                telemetry.update();
-            // End of Raising Foundation Movers
+        telemetry.addData("Speed", Speed);
+        telemetry.addData("Wrist", wrist.getPosition());
+        telemetry.update();
+        // End of Raising Foundation Movers
         /** End of Foundation Mover Controls **/
 
         /** Gamepad 2 Controls (Payload) ==> **/
 
         /** Lift System Controls **/
-            // Moving The Lift Upward
-                if (gamepad2.left_stick_y > .25) {
-                    lift.setPower(-1); //Set power to the slide
-                }
-            // End of Moving the Lift Upward
+        // Moving The Lift Upward
+        if (gamepad2.left_trigger > .25) {
+            lift.setPower(-1); //Set power to the slide
+        }
+        // End of Moving the Lift Upward
 
-            // Zeroing the Lift
-                if (gamepad2.left_stick_y < .25 && gamepad2.left_stick_y > -.25) {
-                    lift.setPower(.0001); //Holds lift in place
-                }
-            // End of Zeroing the lift
+        // Zeroing the Lift
+        if (gamepad2.left_trigger < .25 && gamepad2.right_trigger < .25) {
+            lift.setPower(.0002); //Holds lift in place
+        }
+        // End of Zeroing the lift
 
-            // Moving the Lift Downward
-                if (gamepad2.left_stick_y < -.25) {
-                    lift.setPower(1); //Set power to the slide
-                }
-            //End of Moving the Lift Downward
+        // Moving the Lift Downward
+        if (gamepad2.right_trigger > .25) {
+            lift.setPower(1); //Set power to the slide
+        }
+        //End of Moving the Lift Downward
         /** End of Lift System Controls **/
 
         /** Slide System Controls **/
-            // Moving The Slide Outward
-                if (gamepad2.right_stick_y > .25) {
-                    slide.setPower(-1); //Set power to the slide
-                }
-            // End of Moving the Slide Outward
+        // Moving The Slide Outward
+        if (gamepad2.left_stick_y > .25) {
+            slide.setPower(-1); //Set power to the slide
+        }
+        // End of Moving the Slide Outward
 
-            // Zeroing the Slide
-                if (gamepad2.right_stick_y < .25 && gamepad2.right_stick_y > -.25) {
-                    slide.setPower(0); //Stops power to the slid
-                }
-            // End of Zeroing the Slide
+        // Zeroing the Slide
+        if (gamepad2.left_stick_y < .25 && gamepad2.left_stick_y > -.25) {
+            slide.setPower(0); //Stops power to the slid
+        }
+        // End of Zeroing the Slide
 
-            // Moving the Slide Inward
-                if (gamepad2.right_stick_y < -.25) {
-                    slide.setPower(1); //Set power to the slide
-                }
-            // End of Moving the Slide Inward
+        // Moving the Slide Inward
+        if (gamepad2.left_stick_y < -.25) {
+            slide.setPower(1); //Set power to the slide
+        }
+        // End of Moving the Slide Inward
         /** End of Slide System Controls **/
 
         /** Hand System Controls **/
-            // Wrist Controls
-                if (gamepad2.right_stick_x > 0.1) {
-                    wrist.setPosition(wrist.getPosition() - .025); //Move the wrist right
-                }
+        // Wrist Controls
+        if (gamepad2.right_stick_x > 0.1) {
+            wrist.setPosition(wrist.getPosition() + .0025); //Move the wrist right
+        }
 
-                if (gamepad2.right_stick_x < -0.1) {
-                    wrist.setPosition(wrist.getPosition() + .025); //Move the wrist Left
-                }
+        if (gamepad2.right_stick_x < -0.1) {
+            wrist.setPosition(wrist.getPosition() - .0025); //Move the wrist Left
+        }
 
-                if (gamepad2.right_stick_x < -0.1 && gamepad2.right_stick_x > 0.1) {
-                    wrist.setPosition(wrist.getPosition()); //Set the wrist to the current position
-                }
+        if (gamepad2.right_stick_x < -0.1 && gamepad2.right_stick_x > 0.1) {
+            wrist.setPosition(wrist.getPosition()); //Set the wrist to the current position
+        }
 
-                if (gamepad2.dpad_up){
-                    wrist.setPosition(0.5);
-                }
-            // End of Wrist controls
+        if (gamepad2.dpad_up){
+            wrist.setPosition(0.4);
+        }
+        // End of Wrist controls
 
-            // Grabber Controls
-                if (gamepad2.a) {
-                    grabStone.setPosition(0); //Clamp down with the grabber
-                }
+        // Grabber Controls
+        if (gamepad2.a){
+            grabStone.setPosition(0); //Clamp down with the grabber
+        }
 
-                if (gamepad2.b) {
-                    grabStone.setPosition(1); //Release the grabber
-                }
-            // End of Grabber Controls
+        if (gamepad2.b) {
+            grabStone.setPosition(.75); //Release the grabber
+        }
+        // End of Grabber Controls
         /** End of Hand System Control **/
     }
 }
