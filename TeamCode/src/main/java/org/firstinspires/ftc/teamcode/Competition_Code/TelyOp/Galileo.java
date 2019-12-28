97806a127fdba6a5fd9e7bb4d4794c2c361391c3
DRSS Enterprise <ethanjayde@gmail.com>
2019-12-28 09:19:14 -0500

    12-28-19 Commented out code that made the collector open if it was bumped in button code
    Signed-off-by: DRSS Enterprise <ethanjayde@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/TelyOp/Galileo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition_Code/TelyOp/Galileo.java
@@ -314,12 +314,15 @@ public class Galileo extends LinearOpMode {     // Sets the codes name and sets 
                 grabStone.setPosition(0);   // Closes the grabber
             }
 
+
+
             if (stoneButton.getState() == true && !gamepad2.b) { // Do the following if the stone button and not the "b" button is pressed
                 grabStone.setPosition(0);        // Closes the grabber
-            } else {                              // Do the following if the stone button is not pressed or the "b" button is pressed
+            } /*else {                              // Do the following if the stone button is not pressed or the "b" button is pressed
                 grabStone.setPosition(.6);         // Opens the grabber
-            }
+            }*/
             // End of Grabber Controls
+
             /** End of hand system control **/
 
             /** End of gamepad 2 controls (payload) **/
