d5be2b7628147e67c60e190ec28becfc1609fd9e
DRSS Enterprise <drss.enterprise@gmail.com>
2020-01-24 17:49:22 -0500

    Added capstone maneuvering to all autonomous
    Signed-off-by: DRSS Enterprise <drss.enterprise@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Auto_Red_Foundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Auto_Tests/Auto_Red_Foundation.java
@@ -38,6 +38,10 @@ public class Auto_Red_Foundation extends LinearOpMode {
 		if (step == 1) {                                    //Move forward
 			stepTelemetry();                                //Display Telemetry
 
+			//Move the slide forward, and drop lift
+			encoderSlide(1, 4); // Move the slide forward 4 inches
+			encoderLift(1, -1.375); // Drop the lift downward 1.375 inches
+
 			encoderDrive(.4, 46, 10); //Move forward 46 inches just before the foundation
 
 			step++;
@@ -254,6 +258,28 @@ public class Auto_Red_Foundation extends LinearOpMode {
 
 			robot.lift.setPower(0);
 			robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		}
+	}
+
+	public void encoderSlide (double slideSpeed, double Inches){
+		int newLiftTarget;                                      // Creates the integer "newLiftTarget"
+
+		if (opModeIsActive()) {     // Do the following after the start button has been pressed and until the stop button is pressed
+			newLiftTarget = (robot.slide.getCurrentPosition() + (int) (Inches * robot.COUNTS_PER_SLIDE_INCH));
+
+			robot.slide.setTargetPosition(newLiftTarget);
+
+			robot.slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+			robot.slide.setPower(slideSpeed);
+
+			while (opModeIsActive() && robot.slide.isBusy()) {
+				telemetry.addData("lift position", robot.slide.getCurrentPosition());
+				telemetry.update();
+			}
+
+			robot.slide.setPower(0);
+			robot.slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		}
 	}
 
